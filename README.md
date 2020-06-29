28f41880b7ed5bd2f92cb518f4aaad49148e78d9
Jack Hilscher <jhilscheriv@gmail.com>
2020-06-29 19:01:41 -0400

    added different messages and designation for users joining rooms

---
+++ README.md
@@ -0,0 +1,1 @@
+`npm start` to start server on port 1597
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const webSocketsServerPort = 1597
 const webSocketsServer = require('websocket').server
 const http = require('http')
-const uuid = require('uuid')
+const { v4: uuidv4 } = require('uuid')
 
 const server = http.createServer()
 server.listen(webSocketsServerPort)
@@ -11,23 +11,66 @@ const wsServer = new webSocketsServer({
   httpServer: server
 })
 
-const clients = {}
+const clients = []
 
 wsServer.on('request', (req) => {
-  var userID = uuid()
-  console.log((new Date()) + ' Recieved a new connection from origin ' + req.origin + '.')
+  var userID = uuidv4()
+  // console.log((new Date()) + ' Recieved a new connection from origin ' + req.origin + '.')
 
   const connection = req.accept(null, req.origin)
-  clients[userID] = connection
-  console.log(`connected: ${userID} in ${Object.getOwnPropertyNames(clients)}`)
+  clients.push({connection, userID})
+  connection.sendUTF(JSON.stringify({payload: {userID}}))
+  // console.log(`connected: ${userID} in ${Object.getOwnPropertyNames(clients)}`)
 
   connection.on('message', (message) => {
     if (message.type === 'utf8') {
-      console.log('Received Message: ' + message.utf8Data);
-      for(key in clients){
-        clients[key].sendUTF(message.utf8Data);
-        console.log('sent Message to :', clients[key])
+      // console.log('Received Message: ' + message.utf8Data);
+      let payload = JSON.parse(message.utf8Data).payload
+      console.log(payload)
+
+      if (payload.start && payload.players.length === 0){
+        let client = clients.find(c => c.userID === payload.userID)
+        client.gameData = {numOfPlayers: payload.numOfPlayers, players: payload.players}
+        client.user = payload.user
+        client._id = payload._id
+
+        // gather all clients that are missing players
+        let playersMissingPlayers = clients.filter(cl => cl.gameData.numOfPlayers > cl.gameData.players.length && cl._id !== client._id)
+
+        if (playersMissingPlayers.length === 0){
+          // if there is no room_id, create on
+          client.gameData.roomID = uuidv4()
+          client.gameData.players.push({user: client.user, userID: client.userID})
+
+          client.connection.sendUTF(JSON.stringify({payload: {players: client.gameData.players, roomID: client.gameData.roomID, numOfPlayers: client.gameData.numOfPlayers}}))
+        } else {
+          // select from the beginning of that array
+          let chosenPlayer = playersMissingPlayers[0]
+          // otherwise, add room_id
+          client.gameData.roomID = chosenPlayer.gameData.roomID
+          // update all players with player data
+          chosenPlayer.gameData.players.push({user: client.user, userID: client.userID})
+          chosenPlayer.connection.sendUTF(JSON.stringify({payload: {players: chosenPlayer.gameData.players, roomID: chosenPlayer.gameData.roomID, numOfPlayers: chosenPlayer.gameData.numOfPlayers}}))
+
+          clients.forEach(cl => {
+            if (cl.userID !== chosenPlayer.userID && cl.gameData.roomID === chosenPlayer.gameData.roomID){
+              if (cl.userID === client.userID){
+                cl.gameData.players = [...chosenPlayer.gameData.players]
+              } else {
+                cl.gameData.players.push({user: client.user, userID: client.userID})
+              }
+              // send a message to all members of the room
+              cl.connection.sendUTF(JSON.stringify({payload: {players: chosenPlayer.gameData.players, roomID: chosenPlayer.gameData.roomID, numOfPlayers: chosenPlayer.gameData.numOfPlayers}}))
+            }
+          })
+        }
       }
+      // clients.forEach(client => {
+        // client.connection.sendUTF(message.utf8Data);
+        // console.log(message.utf8Data)
+        // console.log(message.utf8Data.payload)
+        // console.log('sent Message to :', clients[key])
+      // })
     }
 
   })
